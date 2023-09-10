d379414f0db2ec4647bfb775bd754fb4fd98e521
Andrey Vorotnikov <39947159+aVorotnikov@users.noreply.github.com>
2023-09-11 00:08:44 +0300

    Добавлены вспомогательные функции

--- triangulation/triangulation.py
+++ triangulation/triangulation.py
@@ -9,4 +9,23 @@ def input():
     return res
 
 
-print(input())
+def square(triangle):
+    return \
+        triangle[0][0] * triangle[1][1] + \
+        triangle[0][1] * triangle[2][0] + \
+        triangle[1][0] * triangle[2][1] - \
+        triangle[1][1] * triangle[2][0] - \
+        triangle[0][0] * triangle[2][1] - \
+        triangle[1][0] * triangle[0][1]
+
+
+def check_vertex_in_triangle(vertex, triangle):
+    sign = 1 if square(triangle) > 0 else -1
+    return \
+        sign * square((triangle[0], triangle[1], vertex)) > 0 and \
+        sign * square((triangle[1], triangle[2], vertex)) > 0 and \
+        sign * square((triangle[2], triangle[0], vertex)) > 0
+
+
+vertices = input()
+print(vertices)
