faae163abab88f3baa192b29d9001094710b3d3f
Andrey Vorotnikov <39947159+aVorotnikov@users.noreply.github.com>
2023-09-16 23:10:03 +0300

    Триангуляция

--- triangulation/triangulation.py
+++ triangulation/triangulation.py
@@ -27,11 +27,11 @@ def check_vertex_in_triangle(vertex, triangle):
         sign * square((triangle[2], triangle[0], vertex)) >= 0
 
 
-def check_ear(triangle, vertices, ignore_start):
+def check_ear(vertices, ignore_start):
+    triangle = [vertices[i % len(vertices)] for i in range(ignore_start, ignore_start + 3)]
     if square(triangle) < 0:
         return False
     ignore_end = (ignore_start + 2) % len(vertices) + 1
-    print(ignore_start, ignore_end)
     if ignore_start < ignore_end:
         for i in range(0, ignore_start):
             if check_vertex_in_triangle(vertices[i], triangle):
@@ -46,5 +46,26 @@ def check_ear(triangle, vertices, ignore_start):
     return True
 
 
-vertices = input()
-print(vertices)
+def print_triangle(triangle):
+    for i in range(0, len(triangle)):
+        print(triangle[i][0], triangle[i][1], end=" ")
+    print("")
+
+
+def print_triangle_index(vertices, ignore_start):
+    print_triangle([vertices[i % len(vertices)] for i in range(ignore_start, ignore_start + 3)])
+
+
+def triangulation(vertices):
+    if len(vertices) < 3:
+        return
+    while (3 < len(vertices)):
+        for i in range(0, len(vertices)):
+            if check_ear(vertices, i):
+                print_triangle_index(vertices, i)
+                del vertices[(i + 1) % len(vertices)]
+                break
+    print_triangle(vertices)
+
+
+triangulation(input())
