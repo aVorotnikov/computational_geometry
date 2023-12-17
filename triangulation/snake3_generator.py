6f7039b46f8e423cd3bc50aeea2ed4781cd66577
Andrey Vorotnikov <39947159+aVorotnikov@users.noreply.github.com>
2023-12-17 16:36:44 +0300

    Генератор третьей версии змейки

--- triangulation/snake3_generator.py
+++ triangulation/snake3_generator.py
@@ -4,18 +4,18 @@ for x in range(0, snake_len + 1):
     if x % 4 == 0:
         print(x, 0)
     if x % 4 == 1:
-        print(x, 2)
+        print(x, 1)
     if x % 4 == 2:
         print(x, 3)
     if x % 4 == 3:
-        print(x, 2)
+        print(x, 1)
 
 for x in range(snake_len, -1, -1):
     if x % 4 == 0:
         print(x, 1)
     if x % 4 == 1:
-        print(x, 3)
+        print(x, 2)
     if x % 4 == 2:
         print(x, 4)
     if x % 4 == 3:
-        print(x, 3)
+        print(x, 2)
